529268f6bd0c5dba9901407f23d86a4e7bebe656
Ale <alexwaam@yahoo.com>
2025-05-08 20:58:05 +0300

    Fixes

--- lib/src/core/obs_core_extensions.dart
+++ lib/src/core/obs_core_extensions.dart
@@ -22,7 +22,7 @@ export '../../src/core/obs_extensions/obs_string_ext.dart';
 extension StringExtension on String {
   /// Returns a `ObservableString` with [this] `String` as initial value.
   ObservableString get obs => ObservableString(this);
-  ObservableAsync<String> get obsA => ObservableAsync(this);
+  ObservableAsync<String> get obsA => ObservableAsync<String>(this);
 
   ObservableReadOnly<String> get obsReadOnly =>
       ObservableReadOnly<String>(this);
@@ -33,7 +33,7 @@ extension StringExtension on String {
 extension IntExtension on int {
   /// Returns a `ObservableInt` with [this] `int` as initial value.
   ObservableInt get obs => ObservableInt(this);
-  ObservableAsync<int> get obsA => ObservableAsync(this);
+  ObservableAsync<int> get obsA => ObservableAsync<int>(this);
 
   ObservableReadOnly<int> get obsReadOnly => ObservableReadOnly<int>(this);
   ObservableAsync<int> get obsAReadOnly =>
@@ -43,7 +43,7 @@ extension IntExtension on int {
 extension DoubleExtension on double {
   /// Returns a `ObservableDouble` with [this] `double` as initial value.
   ObservableDouble get obs => ObservableDouble(this);
-  ObservableAsync<double> get obsA => ObservableAsync(this);
+  ObservableAsync<double> get obsA => ObservableAsync<double>(this);
 
   ObservableReadOnly<double> get obsReadOnly =>
       ObservableReadOnly<double>(this);
@@ -54,7 +54,7 @@ extension DoubleExtension on double {
 extension BoolExtension on bool {
   /// Returns a `ObservableBool` with [this] `bool` as initial value.
   Observable<bool> get obs => Observable<bool>(this);
-  ObservableAsync<bool> get obsA => ObservableAsync(this);
+  ObservableAsync<bool> get obsA => ObservableAsync<bool>(this);
 
   ObservableReadOnly<bool> get obsReadOnly => ObservableReadOnly<bool>(this);
   ObservableAsync<bool> get obsAReadOnly =>
@@ -64,7 +64,7 @@ extension BoolExtension on bool {
 extension ObservableT<T> on T {
   /// Returns a `Observable` instance with [this] `T` as initial value.
   Observable<T> get obs => Observable<T>(this);
-  ObservableAsync<T> get obsA => ObservableAsync(this);
+  ObservableAsync<T> get obsA => ObservableAsync<T>(this);
 
   ObservableReadOnly<T> get obsReadOnly => ObservableReadOnly<T>(this);
   ObservableAsync<T> get obsAReadOnly =>
@@ -74,7 +74,7 @@ extension ObservableT<T> on T {
 extension ListExtension<T> on List<T> {
   /// Returns a `Observable` instance with [this] `List<T>` as initial value.
   Observable<List<T>> get obs => Observable<List<T>>(this);
-  ObservableAsync<List<T>> get obsA => ObservableAsync(this);
+  ObservableAsync<List<T>> get obsA => ObservableAsync<List<T>>(this);
 
   ObservableReadOnly<List<T>> get obsReadOnly =>
       ObservableReadOnly<List<T>>(this);
