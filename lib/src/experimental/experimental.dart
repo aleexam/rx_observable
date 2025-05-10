fe0e38cdb614f85a7583c8a442128fffe11cf8da
Ale <alexwaam@yahoo.com>
2025-05-10 04:04:31 +0300

    Experimental

--- lib/src/experimental/experimental.dart
+++ lib/src/experimental/experimental.dart
@@ -1,3 +1,5 @@
+
+@Deprecated("Experimental feature, probably better not to use yet")
 class ExperimentalObservableFeatures {
   /// Activate experimental features if true. Only use at your own risk
   /// Activates usage of [Observe], [ObsTrackingContext]
