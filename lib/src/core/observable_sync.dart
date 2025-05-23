27b2447519c61d05024b42b01c2beb29cf258f5a
Ale <alexwaam@yahoo.com>
2025-05-23 04:10:03 +0300

    Refactored/fixed

--- lib/src/core/observable_sync.dart
+++ lib/src/core/observable_sync.dart
@@ -33,12 +33,11 @@ class ObservableReadOnly<T> extends ChangeNotifier implements IObservableSync<T>
   }
 
   late T _value;
-
   late bool _notifyOnlyIfChanged;
 
+  /// if true, listeners will be notified, if new value not equals to old value
+  /// Otherwise, any updated will trigger listeners
   bool get notifyOnlyIfChanged => _notifyOnlyIfChanged;
-
-  final _customListeners = <void Function(T)>[];
 
   /// Set and emit the new value.
   void _updateValue(T newValue) {
@@ -67,52 +66,11 @@ class ObservableReadOnly<T> extends ChangeNotifier implements IObservableSync<T>
 
   @override
   ObservableSubscription<T> listen(void Function(T) listener, {bool fireImmediately = false}) {
-    _customListeners.add(listener);
+    assert(ChangeNotifier.debugAssertNotDisposed(this));
+    listenerWrapper() => listener(_value);
+    addListener(listenerWrapper);
     if (fireImmediately) listener(_value);
-    return ObservableSubscription(() => _customListeners.remove(listener));
-  }
-
-  /// Notifies [listen] listeners and common [ChangeNotifier] listeners
-  /// listen listeners will get callback with value,
-  /// while ChangeNotifier listeners gets VoidCallback
-  /// Call all the registered listeners.
-  ///
-  /// Call this method whenever the object changes, to notify any clients the
-  /// object may have changed. Listeners that are added during this iteration
-  /// will not be visited. Listeners that are removed during this iteration will
-  /// not be visited after they are removed.
-  ///
-  /// Exceptions thrown by listeners will be caught and reported using
-  /// [FlutterError.reportError].
-  ///
-  /// This method must not be called after [dispose] has been called.
-  ///
-  /// Surprising behavior can result when reentrantly removing a listener (e.g.
-  /// in response to a notification) that has been registered multiple times.
-  /// See the discussion at [removeListener].
-  @override
-  void notifyListeners() {
-    for (final listener in _customListeners) {
-      try {
-        listener(_value);
-      } catch (exception, stack) {
-        FlutterError.reportError(FlutterErrorDetails(
-          exception: exception,
-          stack: stack,
-          library: 'rx_observable',
-          context: ErrorDescription(
-              'while dispatching rx_observable notifications for $runtimeType'),
-          informationCollector: () => <DiagnosticsNode>[
-            DiagnosticsProperty<ChangeNotifier>(
-              'The $runtimeType sending rx_observable notification was',
-              this,
-              style: DiagnosticsTreeStyle.errorProperty,
-            ),
-          ],
-        ));
-      }
-    }
-    super.notifyListeners();
+    return ObservableSubscription(() => removeListener(listenerWrapper));
   }
 
   @override
@@ -122,25 +80,37 @@ class ObservableReadOnly<T> extends ChangeNotifier implements IObservableSync<T>
 
   @override
   ObservableReadOnly<R> map<R>(R Function(T value) transform) {
-    final result = Observable<R>(transform(value), notifyOnlyIfChanged: _notifyOnlyIfChanged);
+    final mappedObservable = Observable<R>(transform(value), notifyOnlyIfChanged: _notifyOnlyIfChanged);
 
     final subscription = listen((val) {
-      result.value = transform(val);
+      mappedObservable.value = transform(val);
     });
 
-    _mapSubs.add(DisposableAdapter(() {
+
+    var disposer = DisposableAdapter(() {
+      mappedObservable._onDispose = null;
       subscription.cancel();
-      result.dispose();
-    }));
-    return result;
+      mappedObservable.dispose();
+    });
+
+    mappedObservable._onDispose = () {
+      subscription.cancel();
+      _mapSubs.remove(disposer);
+    };
+
+    _mapSubs.add(disposer);
+    return mappedObservable;
   }
 
   @override
   void dispose() {
-    _customListeners.clear();
+    assert(ChangeNotifier.debugAssertNotDisposed(this));
     for (var cancelable in _mapSubs) {
       cancelable.cancel();
     }
+    _onDispose?.call();
     super.dispose();
   }
+
+  void Function()? _onDispose;
 }
